3dd276c326d1aaed30b6a7bfb93d2deb9fc5379c
Francisco Aguilar Cano <franaguilarcano@gmail.com>
2017-01-13 00:17:57 +0100

    Test añadido al metodo suma.

--- Pruebas git/src/pruebas/Suma.java
+++ Pruebas git/src/pruebas/Suma.java
@@ -7,8 +7,12 @@ public static int suma (int a, int b){
 }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		if (suma(2,1) == 3){
+			System.out.println("Todo bien.");
+		}else{
+			System.out.println("Algo falla.");
 
+		}
 	}
 
 }
